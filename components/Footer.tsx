1c3836dbfc7f093e1da2f7b951de8cdc921d27db
Daniel <Daniel.abreu.leal@gmail.com>
2025-08-24 00:37:20 -0300

    voltando ao zero

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-    <footer className="w-full p-4 mt-12 text-center text-xs text-gray-600 border-t border-gray-800/50">
-        &copy; {new Date().getFullYear()} Grêmio Carcará. Euclides da Cunha.
-    </footer>
-);
-
-export default Footer;
